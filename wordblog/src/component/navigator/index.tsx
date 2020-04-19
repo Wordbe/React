e130b95252349c450d5b8ae623fa148615117550
Wordbe <seonghojin3@gmail.com>
2020-04-19 23:59:27 +0900

    200419 nav update

---
+++ wordblog/src/component/navigator/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./style";
+
+function Navigator() {
+
+  return (
+    <>
+      <S.NavLeft>NavLeft</S.NavLeft>
+      <S.NavFill>NavFill</S.NavFill>
+      <S.NavRight>NavRight</S.NavRight>
+    </>
+  );
+};
+
+export default Navigator;
---
+++ wordblog/src/component/navigator/style.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components/macro";
+
+export const NavLeft = styled.div`
+  position: relative;
+  float: left;
+  width: auto;
+  height: 60px;
+`;
+
+export const NavFill = styled.div`
+  width: auto;
+  height: 60px;
+  overflow: visible;
+`;
+
+export const NavRight = styled.div`
+  position: relative;
+  float: right;
+  width: auto;
+  height: 60px;
+`;
+
