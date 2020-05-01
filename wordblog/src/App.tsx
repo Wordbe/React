9c6aed402be77b4522628ed8d302eb697790750f
Wordbe <seonghojin3@gmail.com>
2020-05-01 23:51:49 +0900

    200501 web viewer added

--- wordblog/src/App.tsx
+++ wordblog/src/App.tsx
@@ -5,6 +5,7 @@ import Main from './view/Main'
 import SignUp from './view/member/SignUp';
 import Footer from './component/footer';
 import SearchResult from './view/search/SearchResult';
+import WebViewer from './view/webviewer/WebViewer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" exact={true} component={Main} />
       <Route path="/sign-up" exact={true} component={SignUp} />
       <Route path="/s" exact={true} component={SearchResult} />
+      <Route path="/webviewer" exact={true} component={WebViewer} />
       <Footer></Footer>
     </>
   );
